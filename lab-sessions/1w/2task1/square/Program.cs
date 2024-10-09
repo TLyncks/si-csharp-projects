673e3dc94b78f779e2c077cd9498e73a147535e9
TLyncks <tairose@btinternet.com>
2024-10-09 21:18:24 +0100

    Inital set up + Week 1 Content

---
+++ lab-sessions/1w/2task1/square/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace square
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("\n*****");
+      Console.WriteLine("*****");
+      Console.WriteLine("*****");
+      Console.WriteLine("*****");
+      Console.WriteLine("*****");
+
+
+      Console.WriteLine("\nPress any key to continue...");
+      Console.ReadKey();
+
+    }
+  }
+}
---
+++ lab-sessions/1w/3task1/tempConv/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; // ensures availability of resources
+
+namespace tempConv // ensures common area of memory
+{
+  class TempConv
+  { 
+    static void Main(string[ ] args) // where execution starts
+    {
+      double fahrTemp, celTemp;
+      string input; // string is a ‘class’ type
+      Console.WriteLine("Enter a temperature in degrees F >");
+      input = Console.ReadLine();
+      fahrTemp = double.Parse( input ); // converts string to double
+      celTemp = (fahrTemp - 32.0) * 5.0/9.0; // conversion
+      Console.WriteLine("The temperature in degrees C is " + celTemp);
+
+    } // brackets define blocks of code and are indented
+  }
+}
